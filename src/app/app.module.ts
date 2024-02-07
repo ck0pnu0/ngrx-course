bc1c45fc63cfa756e34472c08193c7b086bb1aae
ck0pnu0 <georgi.dimitrov89@gmail.com>
2024-02-07 09:42:10 +0200

    ngrx development tools in depth - lecture 24

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,8 +21,8 @@ import {RouterState, StoreRouterConnectingModule} from '@ngrx/router-store';
 import {EffectsModule} from '@ngrx/effects';
 import {EntityDataModule} from '@ngrx/data';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import * as fromApp from './reducers';
 import { AuthGuard } from './auth/auth.guard';
+import { appReducer, metaReducers } from './reducers';
 
 
 const routes: Routes = [
@@ -55,9 +55,21 @@ const routes: Routes = [
     MatListModule,
     MatToolbarModule,
     AuthModule.forRoot(),
-    StoreModule.forRoot(fromApp.appReducer, {}),
+    StoreModule.forRoot(appReducer, { 
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+        strictActionSerializability: true, // doesn't allow "data" prop in the object
+        strictStateSerializability: true
+      }
+    }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-    EffectsModule.forRoot([])
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({
+      stateKey: 'router',
+      routerState: RouterState.Minimal
+    })
   ],
   bootstrap: [AppComponent]
 })
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,4 +1,5 @@
 import { isDevMode } from '@angular/core';
+import { routerReducer } from '@ngrx/router-store';
 import {
   ActionReducer,
   ActionReducerMap,
@@ -8,17 +9,26 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import * as fromAuth from '../auth/reducers';
+import { environment } from '../../environments/environment';
 
 export const appFeatureKey = 'app';
 
 export interface AppState {
-  auth: fromAuth.AuthState;
+
 }
 
-export const initialAppState: AppState = {
-  auth: fromAuth.initialAuthState
+// meta reducer
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log('state before: ', state);
+    console.log('action: ', action);
+
+    return reducer(state, action);
+  }
+}
+
+export const appReducer: ActionReducerMap<AppState> = {
+  router: routerReducer
 };
 
-export const appReducer = createReducer(
-  initialAppState,
-);
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [logger] : [];
